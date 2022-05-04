7fd38f25e796deb4b2547960a7c0977511fd3a11
Alicia Padrón <aliciaapadron@gmail.com>
2022-05-04 16:04:46 +0200

    add Api

---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+const getApiData = () => {
+  return fetch(
+    'https://owen-wilson-wow-api.herokuapp.com/wows/random?results=50'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const dataClean = data.map((movie) => {
+        return {
+          id: movie.movie,
+          poster: movie.poster,
+          name: movie.movie,
+          quote: movie.full_line,
+          year: movie.year,
+        };
+      });
+      return dataClean;
+    });
+};
+export default getApiData;
